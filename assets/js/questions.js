f9d579090fc3cabf58a25681bb913be1bc484305
Robert Taylor <rjt903@gmail.com>
2023-01-15 13:33:38 +0000

    Added placeholder questions

---
+++ assets/js/questions.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        "question": "What does CSS stand for?",
+        "oneFalse": "Cascading Style Sheet",
+        "twoFalse": "Cascading Style Sheet",
+        "Correct": "Cascading Style Sheet"
+    },
+    {
+        "question": "How do you add an item to the end of an array in JavaScript?",
+        "oneFalse": ".append()",
+        "Correct": ".push()",
+        "threeFalse": ".place()"
+    }
+];
