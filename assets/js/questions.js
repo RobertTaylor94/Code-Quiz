57a00e8f49e600d4b627e910d51b5b8bd20ece16
Robert Taylor <rjt903@gmail.com>
2023-01-18 16:14:42 +0000

    Added new questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,5 +8,20 @@ const questionsArray = [
         "question": "How do you add an item to the end of an array in JavaScript?",
         "options": [".append()", ".push()", ".place()"],
         "correct": ".push()"
+    },
+    {
+        "question": "Which HTML element do we put JavaScript in?",
+        "options": ["<js>", "<link>", "<script>"],
+        "correct": "<script>"
+    },
+    {
+        "question": "How would you print a message to the browser console?",
+        "options": ["console.log()", "print()", "console.print()"],
+        "correct": "console.log()"
+    },
+    {
+        "question": "Which CSS display type has a property 'Justify Content'",
+        "options": ["Grid", "Flex", "Block"],
+        "correct": "Flex"
     }
 ];
