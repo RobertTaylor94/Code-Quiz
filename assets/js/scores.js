13c6fb917435d2dc91f50f9dfdf9cfd6c88002c3
Robert Taylor <rjt903@gmail.com>
2023-01-18 14:23:59 +0000

    check for empty scores array before rending list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,15 +4,20 @@ var highscores = document.querySelector("#highscores");
 function clearScores() {
     //clear scores from local storage on a button press
     localStorage.clear();
+    highscores.textContent = ""
 };
 
 function getScores() {
     //get scores from storage to display with initals
     var scores = JSON.parse(localStorage.getItem("scores"));
     // scores.sort((a, b) => a.value - b.value);
-    for (i = 0; i < scores.length; i++) {
-        renderScores(scores[i]);
-    };
+
+    if (scores != null) {
+        for (i = 0; i < scores.length; i++) {
+            renderScores(scores[i]);
+        };
+    }
+
 };
 
 function renderScores(object) {
