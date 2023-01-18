5101f12c25d802692289c1eb5671a41c27f56fbd
Robert Taylor <rjt903@gmail.com>
2023-01-18 13:16:02 +0000

    added clear highscores functionality

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,13 @@
+var clearButton = document.querySelector("#clear")
+
 function clearScores() {
     //clear scores from local storage on a button press
+    localStorage.clear()
 }
 
 function getScores() {
     //get scores from storage to display with initals
+    
 }
+
+clearButton.addEventListener("click", clearScores)
