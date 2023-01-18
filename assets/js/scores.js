d7b0f801f49d3c9442bbbd53e1c3c86808a75c92
Robert Taylor <rjt903@gmail.com>
2023-01-18 13:37:12 +0000

    retrieve array of scores and display in a list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,13 +1,26 @@
-var clearButton = document.querySelector("#clear")
+var clearButton = document.querySelector("#clear");
+var highscores = document.querySelector("#highscores");
 
 function clearScores() {
     //clear scores from local storage on a button press
-    localStorage.clear()
-}
+    localStorage.clear();
+};
 
 function getScores() {
     //get scores from storage to display with initals
-    
-}
+    var scores = JSON.parse(localStorage.getItem("scores"));
+    for (i = 0; i < scores.length; i++) {
+        // renderScores(scores[i]);
+    };
+};
 
-clearButton.addEventListener("click", clearScores)
+function renderScores(object) {
+    var newLi = document.createElement("li");
+    newLi.textContent(`${object.player}: ${object.score}`);
+    highscores.append(newLi);
+};
+
+console.log("hello");
+getScores();
+
+clearButton.addEventListener("click", clearScores);
